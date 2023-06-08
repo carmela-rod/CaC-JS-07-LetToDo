f6a4e3d001b267e4311d11f7d7a7b47345ef59a8
Carmela <carme.rod@hotmail.com>
2023-06-08 14:35:19 -0300

    Render de Tareas

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
-const tareas = [];
+let tareas = [];
 
 const form = document.querySelector("form");
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const inputText = document.querySelector("#text");
-        tareas.push({
-            id: Date.now(),
-            text: inputText.ariaValueMax,
-            complete:false,
-        });
+    tareas.push({
+        id: Date.now(),
+        text: inputText.value,
+        complete: false,
+    });
+
+    localStorage.setItem("tareas", JSON.stringify(tareas));
 
     inputText.value = "";
-    console.log(tareas);
-})
+    renderTareas();
+});
+
+const renderTareas = () => {
+    tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+
+    tareas.forEach(
+        (tarea) =>
+            (tbody.innerHTML += ` 
+            <tr>
+                <td>${tarea.text}</td>
+                <td>    
+                <button>Completar</button>
+                <button>Editar</button>
+                <button>Borrar</button>
+                </td>
+            </tr>   
+            `)
+    );
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    renderTareas();
+});
